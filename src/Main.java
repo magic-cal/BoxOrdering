014f6f8a835ab4802c7501f3fe273737ae74c689
CJ Mcclure <callum.mcclure98@gmail.com>
2018-11-15 17:38:43 +0000

    Inital Jframe Experimentaion

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +14,7 @@
  */
 public class Main {
     public static void main(String args[]){
+    boxOrder order = new boxOrder();
       
     }
 }
