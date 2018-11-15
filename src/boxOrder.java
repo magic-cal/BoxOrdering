4888b08a0b4e1fae62611e217800c0b89539d597
Felix <felixwmayo@gmail.com>
2018-11-15 17:20:58 +0000

    Initilizing class

---
+++ src/boxOrder.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author felixmayo
+ */
+public class boxOrder {
+    private int ID;      // class instance variables
+    private int quantity;
+    // Constructor
+    public boxOrder(int boxID, int boxQuantity) {
+        ID = boxID;
+        quantity = boxQuantity;
+    }
+    
+}
+        
