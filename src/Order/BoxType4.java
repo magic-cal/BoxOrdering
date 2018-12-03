7d90729f1a7567d8029ad477c703e2fb8a73ab26
chtzmre <45268408+chtzmre@users.noreply.github.com>
2018-12-03 05:38:02 +0000

    Create BoxType4.java

---
+++ src/Order/BoxType4.java
@@ -0,0 +1,26 @@
+package Order;
+/**
+ *
+ * @author up827158, up867238
+ */
+
+public class BoxType4 extends Box {
+    private int colourPrint;
+    public BoxType4(float boxSize, int boxGrade, boolean boxSealableTop) {
+        super(boxSize, boxGrade, boxSealableTop);
+        
+        this.colourPrint = 2;
+        
+    }
+
+            @Override
+    public double getCost() {
+        double cost = 0.00;
+        double basicCost = super.getBasicCost();
+//      Adding for the cost of the Colouring and Reinforced Bottom 
+//      15% + 13% added to the Original cost.
+        cost += basicCost*(0.13+0.15+1);
+
+        return cost;
+    }
+}
