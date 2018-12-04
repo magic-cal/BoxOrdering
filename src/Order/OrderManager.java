9da1a3a528495d890e3b275929610a54b7ffa5e2
chtzmre <45268408+chtzmre@users.noreply.github.com>
2018-12-04 12:54:02 +0000

    Update OrderManager.java

--- src/Order/OrderManager.java
+++ src/Order/OrderManager.java
@@ -30,7 +30,7 @@ public class OrderManager {
                     return 1;
                 case 2:
                     System.out.println("box 2");
-                    boxes.add(new BoxType2(size,grade,sealedTop,quantity));
+                    boxes.add(new BoxType2(size, grade, colourPrint, sealedTop, reBottom, reCorners, quantity));
                     return 2;
                 case 3:
                     System.out.println("box 3");
